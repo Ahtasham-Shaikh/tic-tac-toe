4a4d4b3f04a77bd5d5dcb7165970cc0ed09b03f7
Ahtasham-Shaikh <ashaikh2u@gmail.com>
2021-10-26 21:21:06 +0530

    Created checkWinner function

--- tic/src/Board.js
+++ tic/src/Board.js
@@ -5,31 +5,40 @@ import "./board.css"
 
 class Board extends React.Component{
 
-  status = "Next Player: X";
-
   constructor(){
     super();
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      isXNext: true
     }
   }
 
   handler(index){
     const squares = this.state.squares.slice();
-    squares[index] = "X";
+    squares[index] = this.state.isXNext? "X" : "O";
     this.setState({
-      squares: squares
+      squares: squares,
+      isXNext: !(this.state.isXNext)
     })
   }
 
-  createSquare = (i) => <Square
-    value={this.state.squares[i]}
-    onClick={() => this.handler(i)} />
+  createSquare = (i) => 
+    <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handler(i)} 
+    />
 
   render(){
+    const winner = checkWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.isXNext ? 'X' : 'O');
+    }
     return(
       <div>
-        <div className="status">{this.status}</div>
+        <div className="status">{status}</div>
         <div className="row">
           {this.createSquare(0)}
           {this.createSquare(1)}
@@ -52,6 +61,27 @@ class Board extends React.Component{
 
 }
 
+function checkWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
+  for(let i=0; i < lines.length; i++ ){
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
+      console.log("We Have a winner!");
+      return;
+   }else
+      return null
+  }
+
+}
 
 export default Board;
